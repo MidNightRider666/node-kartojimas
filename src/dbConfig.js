13d0b1266d5dda8b2ecc6dc98c3a25e492f43a65
MidNightRider666 <ledas850@gmail.com>
2022-05-05 13:29:11 +0300

    Fix DbConfig

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -4,7 +4,6 @@ const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
   database: process.env.DB_DB,
 };
 
